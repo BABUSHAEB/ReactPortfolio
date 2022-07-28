84ed6159350e3188877525b86a32865012d88584
BABUSHAEB <abhisheksingj123@gmail.com>
2022-07-28 21:28:58 +0530

    Updated Skilss Pages

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -18,11 +18,11 @@ export default function Skills() {
     '@media (max-width: 0px) and(max-width: 500px)': {
       fontSize: "5px", fontWeight: "600"
     },
-   
+
   }
-let heading = {
-  padding: "2rem 0" 
-}
+  let heading = {
+    padding: "2rem 0"
+  }
   return (
     <div className='component-container' style={Container}>
 
@@ -32,67 +32,32 @@ let heading = {
       <div >
         <div className='container gridView'>
           <div>
-            <details>
-              <summary>HTML 5 <i className="fa-brands fa-html5" /></summary>
-              <ul>
-                <li>helll</li>
-                <li>life</li>
-                <li>dance</li>
-              </ul>
-            </details>
+            <span>   HTML 5 <i className="fa-brands fa-html5" /></span>
+
           </div>
           <div className="progress">
             <div className="progress-bar bg-success" role="progressbar" aria-label="Success example" style={{ width: "80%" }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">80%</div>
           </div>
           <div>
-            <details>
-              <summary>CSS <i className="fa-brands fa-css3" /> </summary>
-              <ul>
-                <li>helll</li>
-                <li>life</li>
-                <li>dance</li>
-              </ul>
-            </details>
+            <span>  CSS <i className="fa-brands fa-css3" /> </span>
           </div>
           <div className="progress">
             <div className="progress-bar bg-success" role="progressbar" aria-label="Success example" style={{ width: "80%" }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">80%</div>
           </div>
           <div>
-            <details>
-              <summary> JavaScript <i className="fa-brands fa-js" /> </summary>
-              <ul>
-                <li>helll</li>
-                <li>life</li>
-                <li>dance</li>
-              </ul>
-            </details>
+            <span> JavaScript <i className="fa-brands fa-js" /> </span>
           </div>
           <div className="progress">
             <div className="progress-bar bg-success" role="progressbar" aria-label="Success example" style={{ width: "70%" }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">70%</div>
           </div>
           <div>
-            <details>
-              <summary> Bootstrap <i className="fa-brands fa-bootstrap" /></summary>
-              <ul>
-                <li>helll</li>
-                <li>life</li>
-                <li>dance</li>
-              </ul>
-            </details>
-
+            <span>    Bootstrap <i className="fa-brands fa-bootstrap" /></span>
           </div>
           <div className="progress">
             <div className="progress-bar bg-success" role="progressbar" aria-label="Success example" style={{ width: "75%" }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">75%</div>
           </div>
           <div>
-            <details>
-              <summary>  React <i className="fa-brands fa-react" /></summary>
-              <ul>
-                <li>helll</li>
-                <li>life</li>
-                <li>dance</li>
-              </ul>
-            </details>
+            <span>   React <i className="fa-brands fa-react" /></span>
           </div>
           <div className="progress">
             <div className="progress-bar bg-success" role="progressbar" aria-label="Success example" style={{ width: "60%" }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">60%</div>
@@ -106,53 +71,25 @@ let heading = {
       <div >
         <div className='container gridView' >
           <div>
-            <details>
-              <summary>GIt/GitBash <i className="fa-brands fa-github" /></summary>
-              <ul>
-                <li>helll</li>
-                <li>life</li>
-                <li>dance</li>
-              </ul>
-            </details>
+            <span>  GIt/GitBash <i className="fa-brands fa-github" /></span>
           </div>
           <div className="progress">
             <div className="progress-bar bg-success" role="progressbar" aria-label="Success example" style={{ width: "50%" }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">50%</div>
           </div>
           <div>
-          <details>
-              <summary>Visual Studio Code <i className="fa-brands fa-github" /></summary>
-              <ul>
-                <li>helll</li>
-                <li>life</li>
-                <li>dance</li>
-              </ul>
-            </details>
+            <span> Visual Studio Code <i className="fa-brands fa-github" /></span>
           </div>
           <div className="progress">
             <div className="progress-bar bg-success" role="progressbar" aria-label="Success example" style={{ width: "50%" }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">50%</div>
           </div>
           <div>
-          <details>
-              <summary>Postman <i className="fa-brands fa-github" /></summary>
-              <ul>
-                <li>helll</li>
-                <li>life</li>
-                <li>dance</li>
-              </ul>
-            </details>
+            <span> Postman <i className="fa-brands fa-github" /></span>
           </div>
           <div className="progress">
             <div className="progress-bar bg-success" role="progressbar" aria-label="Success example" style={{ width: "50%" }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">50%</div>
           </div>
           <div>
-          <details>
-              <summary>MS Office <i className="fa-brands fa-github" /></summary>
-              <ul>
-                <li>helll</li>
-                <li>life</li>
-                <li>dance</li>
-              </ul>
-            </details>
+            <span> MS Office <i className="fa-brands fa-github" /></span>
           </div>
           <div className="progress">
             <div className="progress-bar bg-success" role="progressbar" aria-label="Success example" style={{ width: "50%" }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">50%</div>
