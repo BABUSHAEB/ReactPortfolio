4247cacb09d07eef3b7f4eca49d044a76648e6ac
BABUSHAEB <abhisheksingj123@gmail.com>
2022-07-30 23:34:28 +0530

    Academic Section Update

--- src/Components/Academic.js
+++ src/Components/Academic.js
@@ -1,7 +1,108 @@
 import React from 'react'
+import styled from 'styled-components';
+
+
+let AcademicGrid = styled.div`
+display: grid;
+grid-template-columns: auto;
+font-weight: "600";
+grid-gap: 16px;
+justify-content: space-around;
+padding: 1rem;
+
+div.box-shadow{
+  box-shadow: 5px 5px 8px black;
+}
+div.box-shadow: hover{
+    box-shadow: 5px 5px 6px blue;
+ }
+ .card-body{
+  padding:10px;
+ }
+@media (min-width: 0px) and (max-width: 600px){
+ 
+  
+  
+
+}
+`;
 
 export default function Academic() {
+  let Acdemic = [{
+    id: 1,
+    Education: "Graduation",
+    Degree: "Bachelor of Science",
+    Specialization: "Information Technology",
+    Board: "University Of Mumbai",
+    FromYear: 2017,
+    FromYearMonth: "August",
+    ToYear: 2020,
+    ToYearMonth: "November",
+    MarkObtained: "7.65 CGPA",
+    Institute: "Saket College Of Arts Science and Commerce"
+  }, {
+    id: 2,
+    Education: "Intermediate",
+    Degree: "Higher Secondary School/PUC",
+    Specialization: "Science",
+    Board: "Bihar School Examination Board, PATNA Borad",
+    FromYear: 2015,
+    FromYearMonth: "May",
+    ToYear: 2017,
+    ToYearMonth: "May",
+    MarkObtained: "60 %",
+    Institute: "S.K.M.R G. College, Latihanwa, E.Champaran"
+  }, {
+    id: 3,
+    Education: "SSC/Matriculation",
+    Degree: "SLC/10th",
+    Specialization: "Math,Computer",
+    Board: "SLC Borad ",
+    FromYear: 2013,
+    FromYearMonth: "March",
+    ToYear: 2014,
+    ToYearMonth: "June",
+    MarkObtained: "51.2 %",
+    Institute: "Angel International Boarding School"
+  }];
+
   return (
-    <div className='component-container'>Academic</div>
+    <div className='component-container'>
+      <AcademicGrid >
+        {Acdemic.map(Acdemic => {
+          return (
+            <>
+              <div key={Acdemic.id} className="card1" >
+                <div className="box-shadow">
+                  {/* <img src={Acdemic.Image}  className="card-img-top" alt={Acdemic.Title} /> */}
+                  <div className="card-body">
+                    <h4 className="card-title heading" >{Acdemic.Education}</h4>
+                    <p className="card-text my-1 mx-1"><strong>Course: </strong>{Acdemic.Degree}</p>
+                    <p className="card-text my-1 mx-1"><strong>Specialization: </strong>{Acdemic.Specialization}</p>
+                    <p className="card-text my-1 mx-1"><strong>Institute: </strong>{Acdemic.Institute}</p>
+                    <p className="card-text my-1 mx-1"><strong>University: </strong>{Acdemic.Board}</p>
+                    <p className="card-text my-1 mx-1"><strong>Year: </strong>{Acdemic.FromYearMonth}-{Acdemic.FromYear} to {Acdemic.ToYearMonth}-{Acdemic.ToYear}</p>
+                    <p className="card-text my-1 mx-1"><strong>Score: </strong>{Acdemic.MarkObtained}</p>
+                    {/* <strong> Technology: </strong><br /> */}
+                    {/* {Acdemic.Skills.map((Skill, index) => {
+                      return (
+                        <>
+                          <button key={index} className='btn btn-warning my-1 mx-1' style={{ fontSize: "9px" }}>{Skill}</button>
+                        </>
+                      )
+                    })}
+                    <br /> */}
+
+                  </div>
+                </div>
+              </div>
+            </>
+
+          )
+        })}
+      </AcademicGrid>
+
+
+    </div>
   )
 }
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -11,28 +11,56 @@ let PersonalProjectGrid = styled.div`
     grid-gap: 16px;
     justify-content: space-around;
     margin:2rem;
-    padding-top: 2rem;
+    div.box-shadow{
+      box-shadow: 5px 5px 8px black;
+    }
+
+    div.card-body{
+      padding:0.8rem;
+      border-radius:20%;
+    }
+    div.card1{
+      width:24rem;
+      border: none;
+    }
     div.box-shadow: hover{
         box-shadow: 5px 5px 6px blue;
      }
     @media (min-width: 0px) and (max-width: 600px){
       grid-template-columns: auto;
+        div.box-shadow{
+          box-shadow: 5px 5px 6px black;
+          margin-bottom: 2rem;
+      }
+      div.card1{
+        // width:fit-content;
+        width: 92vw;
+      }
     }
 `;
 
 let CollegeProjectGrid = styled.div`
 display: grid;
 grid-template-columns: auto;
-// font-size: 1.5rem;
 font-weight: "600";
 grid-gap: 16px;
 justify-content: space-around;
-margin:2rem;
+padding: 1rem;
+
+div.box-shadow{
+  box-shadow: 5px 5px 8px black;
+}
 div.box-shadow: hover{
     box-shadow: 5px 5px 6px blue;
  }
+ .card-body{
+  padding:10px;
+ }
 @media (min-width: 0px) and (max-width: 600px){
-  grid-template-columns: auto;
+ 
+  
+  
+
 }
 `;
 export default function Projects() {
@@ -54,21 +82,30 @@ export default function Projects() {
     Role: "UI Development",
     Skills: ["JSX", "CSS", "JavaScript", "Bootstrap", "React JS", "React Router"],
     Description: "I have created to Enhance my Skills towards React .The beauty of this project is that it has theme of dark and light ,It is able to perform such operation like Upper Case ,Lower Case ,Count the both letter and word , reverse the Entire paragraph ,Copy all the paragraph in text box remove extra Spaces and Download Text in '.txt' Formate."
+  },{
+    id: 2,
+    Title: "Text Tools",
+    Platform: "React Js Library",
+    Image: "Images/Texttools.png",
+    IDE: "Visual Studio Code",
+    Role: "UI Development",
+    Skills: ["JSX", "CSS", "JavaScript", "Bootstrap", "React JS", "React Router"],
+    Description: "I have created to Enhance my Skills towards React .The beauty of this project is that it has theme of dark and light ,It is able to perform such operation like Upper Case ,Lower Case ,Count the both letter and word , reverse the Entire paragraph ,Copy all the paragraph in text box remove extra Spaces and Download Text in '.txt' Formate."
   }];
   return (
     <div className='component-container'>
       <div>
-        <h2 className="heading">College Projects</h2>
+        <h2 className="heading">College Project</h2>
         <CollegeProjectGrid >
           {CollegeProjects.map(CollegeProjects => {
             return (
               <>
-                <div key={CollegeProjects.id} className="card" style={{ width: "24rem" }}>
+                <div key={CollegeProjects.id} className="card1" >
                   <div className="box-shadow">
                     {/* <img src={CollegeProjects.Image}  className="card-img-top" alt={CollegeProjects.Title} /> */}
                     <div className="card-body">
                       <h4 className="card-title heading" >{CollegeProjects.Title}</h4>
-                      <p className="card-text my-1 mx-1"><strong>Plateform:</strong> {CollegeProjects.Platform}</p>
+                      <p className="card-text my-1 mx-1"><strong>Platform:</strong> {CollegeProjects.Platform}</p>
                       <p className="card-text my-1 mx-1"><strong>IDE:</strong> {CollegeProjects.IDE}</p>
                       <p className="card-text my-1 mx-1"><i>{CollegeProjects.Description}</i></p>
                       <strong> Technology: </strong><br/>
@@ -96,19 +133,18 @@ export default function Projects() {
           {PersonalProjects.map(PersonalProjects => {
             return (
               <>
-                <div key={PersonalProjects.id} className="card" style={{ width: "24rem" }}>
+                <div key={PersonalProjects.id} className="card1" >
                   <div className="box-shadow">
                     <img src={PersonalProjects.Image}  className="card-img-top" alt={PersonalProjects.Title} />
                     <div className="card-body">
                       <h4 className="card-title heading" >{PersonalProjects.Title}</h4>
-                      <p className="card-text my-1 mx-1"><strong>Plateform:</strong> {PersonalProjects.Platform}</p>
+                      <p className="card-text my-1 mx-1"><strong>Platform:</strong> {PersonalProjects.Platform}</p>
                       <p className="card-text my-1 mx-1"><strong>IDE:</strong> {PersonalProjects.IDE}</p>
                       <p className="card-text my-1 mx-1"> <i>{PersonalProjects.Description} </i></p>
                       <strong> Technology: </strong><br/>
                       {PersonalProjects.Skills.map((Skill, index) => {
                         return (
                           <>
-                     
                             <button key={index} className='btn btn-warning my-1 mx-1' style={{ fontSize: "9px" }}>{Skill}</button>
 
                           </>
